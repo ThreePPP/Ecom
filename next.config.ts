eff3691ecf87666959e727b871bfaf95b296388a
ThreePPP <131146098+ThreePPP@users.noreply.github.com>
2025-11-08 21:10:07 +0700

    feat: Add axios dependency and configure image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
